8c4ee053bd0b426087df687065f7fb7d545b3f74
Allan Harper <aluhadora@gmail.com>
2017-07-07 22:03:21 -0600

    added streamers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708033506) do
+ActiveRecord::Schema.define(version: 20170708033940) do
+
+  create_table "streamers", force: :cascade do |t|
+    t.string "name"
+    t.string "token"
+    t.string "code"
+    t.string "game_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
